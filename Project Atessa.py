0eab9aeebdbd6c92b3af77804b9037750c0e998f
inngress <rvr.inngress@gmail.com>
2026-02-19 13:47:41 -0800

    Project Attessa V1.2

--- Project Atessa.py
+++ Project Atessa.py
@@ -11,7 +11,7 @@ memory_db = {}
 
 init(autoreset=True)
 
-def ask(prompt):
+def ask(prompt, wrap_words=10):
 
     history.append("User: " + prompt)
     full_prompt = "\n".join(history) + "\nAssistant:"
